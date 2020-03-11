6bf47047f6afd062170953db85f5d8a747b9ed24
filosofeTHElama <jeremy.lelay@outlook.fr>
2020-03-11 16:00:34 +0000

    Job's done

---
+++ app.js
@@ -0,0 +1,43 @@
+/* Jeremy
+Le Lay
+22483
+https://github.com/filosofeTHElama/fewd_lab_1 */
+
+
+// Q3
+var firstName = 'Jeremy';
+
+// Q4
+var favouriteColor = 'Green';
+
+// Q5
+var city = 'Lumbumbashi';
+var country = 'Congo';
+var favouriteFood = 'Tacos';
+var _city = 'Dublin';
+
+// Q6
+//The final x value is 'Toe'
+
+// Q7
+//The final y value is 'Laurel'
+
+// Q8
+console.log('Name: ' + firstName + ', Color: ' + favouriteColor + ', Food: ' + favouriteFood + ', City: ' + _city);
+
+// Q9
+// This line will display on the console the Name, the Color, the Food and the City.
+
+// Q10
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bendingyou see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+// Q11
+console.log(phrase + ' ' + one + ', ' + two + ', ' + three + ', ' + four + ', ' + five + ', ' + six + ', ' + seven + ' ' + endingPhrase);
